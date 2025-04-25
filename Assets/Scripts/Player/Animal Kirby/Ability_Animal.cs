716d518c4f7aa87468ffc7fe5d7fba393b5d2b4f
박민규 <41883207+mingyu8789@users.noreply.github.com>
2025-04-25 16:43:20 +0900

    Animal Kirby File Move

---
+++ Assets/Scripts/Player/Animal Kirby/Ability_Animal.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework.Internal;
+using UnityEngine;
+
+public class Ability_Animal : PlayerAbility
+{
+    public RuntimeAnimatorController animalKirby;
+    
+    public override void OnAbilityCopied(Player owner) //변신을 적용합니다.
+    {
+        base.OnAbilityCopied(owner);
+        animalKirby = Resources.Load<RuntimeAnimatorController>("Test/Animal_Kirby"); //바꿀 애니메이터 파일을 찾아 저장합니다.
+        owner.GetComponentInChildren<Animator>().runtimeAnimatorController = animalKirby; //플레이어에게 애니메이터 파일을 바꿔넣습니다.
+        Debug.Log("Animal ability copied");
+    }
+
+    public override void OnAbilityDestroyed(Player owner) //변신 초기화 입니다.
+    {
+        base.OnAbilityDestroyed(owner); //커비 변신 초기화
+        Debug.Log("Animal ability destroyed");
+    }
+    public override void AttackHandle()
+    {
+        Debug.Log("Animal ability attack");
+    }
+}
--- Assets/Scripts/Player/Eat Kirby/Ability_Eat.cs
+++ Assets/Scripts/Player/Eat Kirby/Ability_Eat.cs
@@ -4,15 +4,15 @@ public class Ability_Eat : PlayerAbility
 {
     public RuntimeAnimatorController EatKirby;
 
-    public override void OnAbilityCopied(Player owner)
+    public override void OnAbilityCopied(Player owner) //변신을 적용합니다.
     {
         base.OnAbilityCopied(owner);
-        EatKirby = Resources.Load<RuntimeAnimatorController>("Test/Eat_Kirby");
-        owner.GetComponentInChildren<Animator>().runtimeAnimatorController = EatKirby;
+        EatKirby = Resources.Load<RuntimeAnimatorController>("Test/Eat_Kirby"); //바꿀 애니메이터 파일을 찾아 저장합니다.
+        owner.GetComponentInChildren<Animator>().runtimeAnimatorController = EatKirby; //플레이어에게 애니메이터 파일을 바꿔넣습니다.
         Debug.Log("Animal ability copied");
     }
 
-    public override void OnAbilityDestroyed(Player owner)
+    public override void OnAbilityDestroyed(Player owner) //변신 초기화 입니다.
     {
         base.OnAbilityDestroyed(owner);
         owner.KirbyFormNum = 0; //커비 변신 초기화
